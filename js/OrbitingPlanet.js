9ce2e12e2b4e31d1218a21cb06fd97f19b0ac520
Fraser Hart <fraser@fraser-hart.co.uk>
2021-03-07 13:14:06 +0000

    horribly adding the ring

--- js/OrbitingPlanet.js
+++ js/OrbitingPlanet.js
@@ -6,10 +6,17 @@ class OrbitingPlanet extends HTMLElement {
   connectedCallback() {
     const planetFriendlyName = this.getAttribute("name");
     const planetName = planetFriendlyName.replaceAll(" ", "-").toLowerCase();
-    const diameter = `${this.getAttribute("diameter")}px`;
+    const diameterRaw = this.getAttribute("diameter");
+    const diameter = `${diameterRaw}px`;
+    const ringDiameterRaw = diameterRaw * 1.4;
+    const ringDiameter = `${ringDiameterRaw}px`;
     const orbitDiameter = `${this.getAttribute("orbitDiameter")}%`;
     const yearDuration = `${this.getAttribute("yearDuration")}s`;
     const colour = this.getAttribute("colour");
+    const ringOffsetRaw = (ringDiameterRaw - diameterRaw) / 2;
+    const ringOffset = `-${ringOffsetRaw}px`;
+    const rings = this.getAttribute("rings") ? "<div class='ring'></div>" : "";
+
     this.innerHTML = `
       <style>
       
@@ -64,9 +71,21 @@ class OrbitingPlanet extends HTMLElement {
           background-color: ${colour};
         }
 
+        .${planetName} .ring {
+
+          position: absolute;
+          margin-left: ${ringOffset};
+          margin-top: ${ringOffset};
+          width: ${ringDiameter};
+          height: ${ringDiameter};
+          border: 1px solid brown;
+          border-radius: 100%
+        }
+
       </style>
       <div class="${planetName} orbit">
         <div class="planet">
+          ${rings}
           <div class="planet-name">${planetFriendlyName}</div>
         </div>
       </div>`;
